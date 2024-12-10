9dd933cc87eb3bc9602a2999f73f48eb560465b1
NZIMA Ivan <nzimaivan007@gmail.com>
2024-12-10 19:38:46 +0100

    wip, product detail slice

--- slices/ProjectDetails/index.vue
+++ slices/ProjectDetails/index.vue
@@ -18,7 +18,22 @@ defineProps(
     :data-slice-type="slice.slice_type"
     :data-slice-variation="slice.variation"
   >
-    Placeholder component for project_details (variation: {{ slice.variation }})
-    Slices
+    <div class="text-stroke-primary w-full h-full flex flex-col">
+      <div class="w-full">
+        <h1 class="heading-h1 w-full">
+          {{ slice.primary.title }}
+        </h1>
+        <hr class="stroke w-full mt-7">
+      </div>
+      {{ slice.primary.summary }}
+      <PrismicLink :field="slice.primary.categories">
+        {{ slice.primary.categories }}
+      </PrismicLink>
+      {{ slice.primary.published_date }}
+      <template v-for="item in slice.primary.others_details">
+        {{ item.type }}
+        {{ item.value }}
+      </template>
+    </div>
   </section>
 </template>
